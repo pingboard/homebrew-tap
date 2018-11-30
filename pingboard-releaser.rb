909d12f3265898ae7d35a0e41ecf23720a7f207b
benfb <bennettbailey@gmail.com>
2018-11-30 11:49:23 -0600

    Brew formula update for pingboard-releaser version v0.1.1

--- pingboard-releaser.rb
+++ pingboard-releaser.rb
@@ -1,9 +1,9 @@
 class PingboardReleaser < Formula
   desc "A tool to help automate Pingboard releases."
   homepage ""
-  url "https://github.com/pingboard/pingboard-releaser/releases/download/v0.1.0/pingboard-releaser_0.1.0_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.1.0"
-  sha256 "c14e66adaf53bcb0cba8949b46c5f1beec8e6636746438056dd0e40ede1711e1"
+  url "https://github.com/pingboard/pingboard-releaser/releases/download/v0.1.1/pingboard-releaser_0.1.1_Darwin_x86_64.tar.gz", :using => GitDownloadStrategy
+  version "0.1.1"
+  sha256 "8a7d099eaed2331d8ab06c785927d2da48382bb82b822c5e2e5308b30338c7a8"
 
   def install
     bin.install "pingboard-releaser"
